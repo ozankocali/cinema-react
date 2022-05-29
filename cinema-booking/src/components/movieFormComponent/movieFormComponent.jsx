47f8bfd0ff84009ea4b770d4e22298befc05e183
ozankocali <ozan.kocali@hotmail.com>
2022-05-29 16:57:21 +0300

    movie form css added

--- cinema-booking/src/components/movieFormComponent/movieFormComponent.jsx
+++ cinema-booking/src/components/movieFormComponent/movieFormComponent.jsx
@@ -31,45 +31,55 @@ const MovieFormComponent = (props) => {
   }, [inputs]);
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Enter Movie ID:
-        <input
-          type="text"
-          name="id"
-          value={inputs.id || ""}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Enter Movie Name:
-        <input
-          type="text"
-          name="name"
-          value={inputs.name || ""}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Enter Movie Genre:
-        <input
-          type="text"
-          name="genre"
-          value={inputs.genre || ""}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Enter Movie Image:
-        <input
-          type="text"
-          name="image"
-          value={inputs.image || ""}
-          onChange={handleChange}
-        />
-      </label>
-      <input type="submit" />
-    </form>
+    <div className="container">
+      <h1 style={{"textAlign":"center"}}>Add New Movie</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Enter Movie ID:</label>
+          <input
+            className="form-control"
+            type="text"
+            name="id"
+            value={inputs.id || ""}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Enter Movie Name:</label>
+          <input
+            className="form-control"
+            type="text"
+            name="name"
+            value={inputs.name || ""}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Enter Movie Genre:</label>
+
+          <input
+            className="form-control"
+            type="text"
+            name="genre"
+            value={inputs.genre || ""}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Enter Movie Image:</label>
+          <input
+            className="form-control"
+            type="text"
+            name="image"
+            value={inputs.image || ""}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit" class="btn btn-primary">
+          Send
+        </button>
+      </form>
+    </div>
   );
 };
 
