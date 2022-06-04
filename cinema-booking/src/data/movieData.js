02aec1f3b1f7604e1612caaa32fc6127feb88ddb
ozankocali <ozan.kocali@hotmail.com>
2022-06-04 15:12:02 +0300

    mkore movies added

--- cinema-booking/src/data/movieData.js
+++ cinema-booking/src/data/movieData.js
@@ -6,6 +6,7 @@ export const movies = [
     image:
       "https://m.media-amazon.com/images/M/MV5BNTYxOTYyMzE3NV5BMl5BanBnXkFtZTcwOTMxNDY3Mw@@._V1_.jpg",
     ticketPrice: 30,
+    director:"Frank Darabont"
   },
   {
     id: "2",
@@ -14,6 +15,7 @@ export const movies = [
     image:
       "https://m.media-amazon.com/images/M/MV5BYWNlN2U4YjQtMzI3NC00ZjkxLWEwMTItYWRlNDUxYTYwYjVlXkEyXkFqcGdeQWpvaG5oYXJ0._V1_QL75_UX500_CR0,0,500,281_.jpg",
     ticketPrice: 30,
+    director:"Francis Ford Coppola"
   },
   {
     id: "3",
@@ -22,5 +24,27 @@ export const movies = [
     image:
       "https://m.media-amazon.com/images/M/MV5BMjAyMjUyNzg1Ml5BMl5BanBnXkFtZTgwNzAwMzg5MTE@._V1_.jpg",
     ticketPrice: 30,
+    director:"Quentin Tarantino"
+    
+  },
+
+  {
+    id: "4",
+    name: "Fight Club",
+    genre: "Drama",
+    image:
+      "https://i.haberglobal.com.tr/rcman/Cw1600h900q95gm/storage/files/images/2022/01/25/cinden-fight-cluba-yeni-son-suclular-yakalandi-cpFT.jpg",
+    ticketPrice: 30,
+    director:"David Fincher"
+    
+  },
+  {
+    id: "5",
+    name: "Ice Age",
+    genre: "Animation",
+    image:"https://images.mubicdn.net/images/film/3387/cache-34479-1445895742/image-w1280.jpg",
+    ticketPrice: 30,
+    director:"Carlos Saldanha"
+    
   },
 ];
